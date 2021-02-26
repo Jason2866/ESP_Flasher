a617b3169b87d15add06094f994348cbcd2c90dc
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-02-26 09:56:19 +0100

    change source path
    for partitions.bin  Path maybe changes...

--- esp_flasher/const.py
+++ esp_flasher/const.py
@@ -5,7 +5,7 @@ __version__ = "1.0.0"
 ESP32_DEFAULT_OTA_DATA = 'https://github.com/espressif/arduino-esp32/raw/1.0.5-rc6/tools/partitions/boot_app0.bin'
 ESP32_DEFAULT_BOOTLOADER_FORMAT = 'https://github.com/espressif/arduino-esp32/raw/' \
                                   '1.0.5-rc6/tools/sdk/bin/bootloader_$FLASH_MODE$_$FLASH_FREQ$.bin'
-ESP32_DEFAULT_PARTITIONS = 'https://github.com/arendst/Tasmota/raw/master/tools/Esptool/ESP32/partitions.bin'
+ESP32_DEFAULT_PARTITIONS = 'https://github.com/Jason2866/ESP_Flasher/raw/main/partitions.bin'
 
 # https://stackoverflow.com/a/3809435/8924614
 HTTP_REGEX = re.compile(r'https?://(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)')
