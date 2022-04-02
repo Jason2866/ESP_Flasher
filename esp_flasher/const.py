89320af0f42a9eb18433cac8a92299538d2cc473
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-04-02 16:37:48 +0200

    Update const.py

--- esp_flasher/const.py
+++ esp_flasher/const.py
@@ -1,11 +1,17 @@
 import re
 
-__version__ = "1.3.1"
+__version__ = "1.4.0"
 
-ESP32_DEFAULT_OTA_DATA = 'https://github.com/espressif/arduino-esp32/raw/1.0.6/tools/partitions/boot_app0.bin'
-ESP32_DEFAULT_BOOTLOADER_FORMAT = 'https://github.com/espressif/arduino-esp32/raw/' \
-                                  '1.0.6/tools/sdk/bin/bootloader_$FLASH_MODE$_$FLASH_FREQ$.bin'
-ESP32_DEFAULT_PARTITIONS = 'https://github.com/Jason2866/ESP_Flasher/raw/main/partitions.bin'
+ESP32_DEFAULT_OTA_DATA = "https://github.com/espressif/arduino-esp32/raw/1.0.6/tools/partitions/boot_app0.bin"
+ESP32_DEFAULT_BOOTLOADER_FORMAT = (
+    "https://raw.githubusercontent.com/espressif/arduino-esp32/"
+    "2.0.2/tools/sdk/$MODEL$/bin/bootloader_$FLASH_MODE$_$FLASH_FREQ$.bin"
+)
+ESP32_DEFAULT_PARTITIONS = (
+    "https://raw.githubusercontent.com/Jason2866/ESP_Flasher/main/partitions.$MODEL$.bin"
+)
 
 # https://stackoverflow.com/a/3809435/8924614
-HTTP_REGEX = re.compile(r'https?://(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)')
+HTTP_REGEX = re.compile(
+    r"https?://(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)"
+)
