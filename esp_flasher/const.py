3cfdb1d9ac7ddc60b0ef7e9acc38f93fa1ee99d2
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-11-17 14:03:23 +0100

    change repo branch

--- esp_flasher/const.py
+++ esp_flasher/const.py
@@ -1,17 +1,17 @@
 import re
 
-__version__ = "2.1.0"
+__version__ = "2.2.0"
 
 ESP32_DEFAULT_OTA_DATA = (
-    "https://raw.githubusercontent.com/Jason2866/ESP_Flasher/C2_test/"
+    "https://raw.githubusercontent.com/Jason2866/ESP_Flasher/factory/"
     "partitions/boot_app0.bin"
 )
 ESP32_DEFAULT_BOOTLOADER_FORMAT = (
-    "https://raw.githubusercontent.com/Jason2866/ESP_Flasher/C2_test/"
+    "https://raw.githubusercontent.com/Jason2866/ESP_Flasher/factory/"
     "bootloader/$MODEL$/bin/bootloader_$FLASH_MODE$_$FLASH_FREQ$.elf"
 )
 ESP32_DEFAULT_PARTITIONS = (
-    "https://raw.githubusercontent.com/Jason2866/ESP_Flasher/C2_test/"
+    "https://raw.githubusercontent.com/Jason2866/ESP_Flasher/factory/"
     "partitions/partitions.$MODEL$.bin"
 )
 ESP32_SAFEBOOT_SERVER = (
