42c306c2b3375b94854c11223bd0f736f54605cf
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2026-04-04 15:14:49 +0200

    Add progress bar display option in own_esptool.py

--- esp_flasher/own_esptool.py
+++ esp_flasher/own_esptool.py
@@ -5658,16 +5658,17 @@ def write_flash(esp, args):
         timeout = DEFAULT_TIMEOUT
 
         while len(image) > 0:
-            pct = 100 * (seq + 1) // blocks
-            bar_length = 30
-            filled = int(bar_length * (seq + 1) / blocks)
-            fill_color = '\033[32m' if pct == 100 else '\033[36m'
-            dim_color = '\033[30m'
-            reset = '\033[0m'
-            bar = fill_color + '█' * filled + dim_color + '█' * (bar_length - filled) + reset
-            bar_str = '%s[%s] %d%% 0x%08x%s' % (fill_color, bar, pct, address + bytes_written, reset)
-            sys.stdout.write(bar_str + ("\n" if pct == 100 else "\r"))
-            sys.stdout.flush()
+            if not getattr(args, 'no_progress', False):
+                pct = 100 * (seq + 1) // blocks
+                bar_length = 30
+                filled = int(bar_length * (seq + 1) / blocks)
+                fill_color = '\033[32m' if pct == 100 else '\033[36m'
+                dim_color = '\033[30m'
+                reset = '\033[0m'
+                bar = fill_color + '█' * filled + dim_color + '█' * (bar_length - filled) + reset
+                bar_str = '%s[%s] %d%% 0x%08x%s' % (fill_color, bar, pct, address + bytes_written, reset)
+                sys.stdout.write(bar_str + ("\n" if pct == 100 else "\r"))
+                sys.stdout.flush()
             block = image[0:esp.FLASH_WRITE_SIZE]
             if compress:
                 # feeding each compressed block into the decompressor lets us see block-by-block how much will be written
